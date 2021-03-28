ab9c28b660013cb54983bc4c1132ea1a5a99651d
Vagner Gross Klaus <vagnergrossklaus@gmail.com>
2021-03-28 15:48:47 -0300

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,160 @@
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+/** We need this to easily convert between pixel and real-world coordinates*/
+static const float SCALE = 30.f;
+
+/** Create the base for the boxes to land */
+void CreateGround(b2World& World, float X, float Y);
+
+/** Create the boxes */
+void CreateBox(b2World& World, int MouseX, int MouseY);
+
+/** Create the boxes */
+void CreateBullet(b2World& World, int MouseX, int MouseY);
+
+int main() {
+	/** Prepare the window */
+	sf::RenderWindow Window(sf::VideoMode(800, 600, 32), "Test");
+	Window.setFramerateLimit(60);
+
+	/** Prepare the world */
+	b2Vec2 Gravity(0.f, 9.8f);
+	b2World World(Gravity);
+	CreateGround(World, 400.f, 500.f);
+
+	/** Prepare textures */
+	sf::Texture GroundTexture;
+	sf::Texture BoxTexture;
+	sf::Texture BulletTexture;
+	GroundTexture.loadFromFile("ground.png");
+	BoxTexture.loadFromFile("box.png");
+	BulletTexture.loadFromFile("box2.png");
+
+	int boxCount = 0;
+
+	while (Window.isOpen()) {
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+
+			int MouseX = sf::Mouse::getPosition(Window).x;
+			int MouseY = sf::Mouse::getPosition(Window).y;
+			if (!boxCount) {
+				CreateBox(World, MouseX, MouseY);
+				boxCount++;
+			}
+		}
+
+		for (b2Body* BodyIterator = World.GetBodyList(); BodyIterator != 0;
+				BodyIterator = BodyIterator->GetNext()) {
+			if (BodyIterator->GetType() == b2_dynamicBody) {
+
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+					BodyIterator->ApplyLinearImpulse(b2Vec2(0.5f, 0),
+							BodyIterator->GetWorldCenter(), true);
+				} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+					BodyIterator->ApplyLinearImpulse(b2Vec2(-0.5f, 0),
+							BodyIterator->GetWorldCenter(), true);
+				} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+
+					BoxTexture.loadFromFile("box_jump.png");
+
+					if (BodyIterator->GetPosition().y >= 15) {
+						BodyIterator->ApplyLinearImpulse(b2Vec2(0, -0.5f),
+								BodyIterator->GetWorldCenter(), true);
+					}
+
+				} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+					BodyIterator->ApplyLinearImpulse(b2Vec2(0, 0.5f),
+							BodyIterator->GetWorldCenter(), true);
+				} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+					CreateBullet(World, SCALE * BodyIterator->GetPosition().x,
+							SCALE * BodyIterator->GetPosition().y);
+				} else {
+					BoxTexture.loadFromFile("box.png");
+				}
+
+			}
+
+		}
+
+		World.Step(1 / 60.f, 8, 3);
+
+		Window.clear(sf::Color::White);
+		int BodyCount = 0;
+		for (b2Body* BodyIterator = World.GetBodyList(); BodyIterator != 0;
+				BodyIterator = BodyIterator->GetNext()) {
+			if (BodyIterator->GetType() == b2_dynamicBody) {
+				sf::Sprite Sprite;
+				Sprite.setTexture(BoxTexture);
+				Sprite.setOrigin(16.f, 16.f);
+				Sprite.setPosition(SCALE * BodyIterator->GetPosition().x,
+						SCALE * BodyIterator->GetPosition().y);
+				Sprite.setRotation(BodyIterator->GetAngle() * 180 / b2_pi);
+				Window.draw(Sprite);
+				++BodyCount;
+			} else {
+				sf::Sprite GroundSprite;
+				GroundSprite.setTexture(GroundTexture);
+				GroundSprite.setOrigin(400.f, 8.f);
+				GroundSprite.setPosition(BodyIterator->GetPosition().x * SCALE,
+						BodyIterator->GetPosition().y * SCALE);
+				GroundSprite.setRotation(
+						180 / b2_pi * BodyIterator->GetAngle());
+				Window.draw(GroundSprite);
+			}
+		}
+		Window.display();
+	}
+
+	return 0;
+}
+
+void CreateBox(b2World& World, int MouseX, int MouseY) {
+	b2BodyDef BodyDef;
+	BodyDef.position = b2Vec2(MouseX / SCALE, MouseY / SCALE);
+	BodyDef.type = b2_dynamicBody;
+	b2Body* Body = World.CreateBody(&BodyDef);
+
+	b2PolygonShape Shape;
+	Shape.SetAsBox((32.f / 2) / SCALE, (32.f / 2) / SCALE);
+	b2FixtureDef FixtureDef;
+	FixtureDef.density = 1.f;
+	FixtureDef.friction = 0.7f;
+	FixtureDef.shape = &Shape;
+	Body->CreateFixture(&FixtureDef);
+}
+
+void CreateBullet(b2World& World, int MouseX, int MouseY) {
+	b2BodyDef BodyDef;
+	BodyDef.position = b2Vec2(MouseX / SCALE, MouseY / SCALE);
+	BodyDef.type = b2_dynamicBody;
+	BodyDef.bullet = true;
+	b2Body* Body = World.CreateBody(&BodyDef);
+
+	b2PolygonShape Shape;
+	Shape.SetAsBox((32.f / 2) / SCALE, (32.f / 2) / SCALE);
+	b2FixtureDef FixtureDef;
+	FixtureDef.density = 1.f;
+	FixtureDef.friction = 0.7f;
+	FixtureDef.shape = &Shape;
+	Body->CreateFixture(&FixtureDef);
+}
+
+void CreateGround(b2World& World, float X, float Y) {
+	b2BodyDef BodyDef;
+	BodyDef.position = b2Vec2(X / SCALE, Y / SCALE);
+	BodyDef.type = b2_staticBody;
+	b2Body* Body = World.CreateBody(&BodyDef);
+
+	b2PolygonShape Shape;
+	Shape.SetAsBox((800.f / 2) / SCALE, (16.f / 2) / SCALE);
+	b2FixtureDef FixtureDef;
+	FixtureDef.density = 0.f;
+	FixtureDef.shape = &Shape;
+	Body->CreateFixture(&FixtureDef);
+}
